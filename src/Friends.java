3e0ad20280615d2fb67bd25e87463b4ee79828c0
Elby <elby223@gmail.com>
2014-12-02 21:28:23 -0500

    Added reader functionality to main()

--- src/Friends.java
+++ src/Friends.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 
+//Authors: Alex Rossi and Elbert Basolis
 public class Friends {
-
+	public static void main(String args[]){ 
+		System.out.println("Enter the filename you want to build the graph with:");
+		Scanner sc = new Scanner(System.in);
+		String file = sc.next();
+		System.out.println(file);
+		
+	}
 }
